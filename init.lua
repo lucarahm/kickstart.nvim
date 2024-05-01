d5d359e9585fc22d551154e64d61b2c839502b3c
Luca Rahm <lucarahm@web.de>
2024-05-01 23:41:20 +0200

    added java to lsp

--- init.lua
+++ init.lua
@@ -568,6 +568,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
+        java_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
